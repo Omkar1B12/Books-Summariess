088583d249270747a676e349a3430df3048c1c1c
Omkar Phadte <omkarphadte64@gmail.com>
2024-11-16 11:52:08 +0530

    Navbar is added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Coverpage from './Components/Coverpage'
 
 function App() {
   return (
     <>
-    
+    <Navbar/>
+    <Coverpage/>
     </>
   )
 }
---
+++ Frontend/src/Components/Coverpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Coverpage() {
+  return (
+    <>
+    <div>
+
+    </div>
+    </>
+  )
+}
+
+export default Coverpage;
