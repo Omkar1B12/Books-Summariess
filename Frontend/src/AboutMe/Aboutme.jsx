ca4533137ec7a51b0c7f787332cb3e030c6e90e3
Omkar Phadte <omkarphadte64@gmail.com>
2024-11-19 11:21:24 +0530

    Homepage image is added

---
+++ Frontend/src/AboutMe/Aboutme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Aboutme() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Aboutme
--- Frontend/src/Components/Coverpage.jsx
+++ Frontend/src/Components/Coverpage.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import banner from "../Images/homepage.jpg"
 function Coverpage() {
   return (
     <>
-    <div>
+<div className="relative inline-block">
+  <img src={banner} alt="Banner" className="w-full h-auto" />
+  
+  {/* Heading */}
+  <h1 
+    className="absolute top-1/3 left-1/2 
+    transform -translate-x-1/2 
+    text-white text-4xl font-serif text-center"
+  >
+    Instant Book Summaries at Your Fingertips
+  </h1>
+  
+  {/* Button */}
+  <button 
+    className="absolute top-1/2 left-1/2 
+    transform -translate-x-1/2 
+    bg-orange-500 text-white px-6 py-3 
+    rounded hover:bg-orange-600 font-semibold"
+  >
+    Explore Various Book Summaries
+  </button>
+</div>
 
-    </div>
+
     </>
   )
 }
